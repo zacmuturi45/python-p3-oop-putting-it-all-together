4bb358c4f956ffaddba4bdd248b84e77db42bc3d
isaacgichuhi <zacmuturi45@gmail.com>
2023-12-05 12:28:07 +0300

    Completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = page_count
+
+    def get_book(self):
+        return self._page_count
+
+    def set_book(self, new_count):
+        if type(new_count) != int:
+            print("page_count must be an integer")
+        else:
+            self._page_count = new_count
+
+    page_count = property(get_book, set_book,)    
         
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, new_size):
+        if type(new_size) == int:
+            self._size = new_size
+        else:
+            print("size must be an integer")
+
+    size = property(get_size, set_size,)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
